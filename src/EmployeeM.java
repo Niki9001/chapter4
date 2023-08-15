9db79310729050527861d23b8a0139a1f22247da
Niki9001 <63759674zxn@gmail.com>
2023-08-15 12:57:17 -0300

    use method to print employee info

---
+++ src/EmployeeM.java
@@ -0,0 +1,12 @@
+public class EmployeeM {
+    int id;
+    String name;
+    int age;
+    double salary;
+    public void id () {
+        System.out.println(id);
+    }
+    public void name(){
+        System.out.println(name);
+    }
+}
---
+++ src/EmployeeMTest.java
@@ -0,0 +1,7 @@
+public class EmployeeMTest {
+    public static void main(String[] args) {
+        EmployeeM em = new EmployeeM();
+        em.id = 01;
+        em.id();
+    }
+}
